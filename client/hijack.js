1eefac85dcc789bf8283303693a8252d048789cf
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-07-11 03:09:53 +0530

    better hijacking for clean stacktrace

--- client/hijack.js
+++ client/hijack.js
@@ -5,60 +5,25 @@ var ConnectionProto = getConnectionProto();
  * Hijack method calls
  */
 var original_Connection_apply = ConnectionProto.apply;
-ConnectionProto.apply = function () {
-  var args = Array.prototype.slice.call(arguments);
-  if(args.length) {
-    var callback = args[args.length - 1];
-    if(typeof callback === 'function') {
-      args[args.length - 1] = zone.bind(callback);
-      original_Connection_apply.apply(this, args);
-    };
-  };
-};
+ConnectionProto.apply = hijackConnection(original_Connection_apply);
+
+// for better stackTraces
+var originalMeteorCall = Meteor.call;
+Meteor.call = hijackConnection(originalMeteorCall);
 
 /*
  * Hijack DDP subscribe method
  * Used when connecting to external DDP servers
  */
 var original_Connection_subscribe = ConnectionProto.subscribe;
-ConnectionProto.subscribe = function () {
-  var args = Array.prototype.slice.call(arguments);
-  if(args.length) {
-    var callback = args[args.length - 1];
-    if(typeof callback === 'function') {
-      args[args.length - 1] = zone.bind(callback);
-    } else if(callback) {
-      ['onReady', 'onError'].forEach(function (funName) {
-        if(typeof callback[funName] === "function") {
-          callback[funName] = zone.bind(callback[funName]);
-        };
-      });
-    }
-    original_Connection_subscribe.apply(this, args);
-  }
-}
+ConnectionProto.subscribe = hijackSubscribe(original_Connection_subscribe);
 
 /**
  * Hijack Meteor.subscribe because Meteor.subscribe binds to
  * Connection.subscribe before the hijack
  */
 var original_Meteor_subscribe = Meteor.subscribe;
-Meteor.subscribe = function () {
-  var args = Array.prototype.slice.call(arguments);
-  if(args.length) {
-    var callback = args[args.length - 1];
-    if(typeof callback === 'function') {
-      args[args.length - 1] = zone.bind(callback);
-    } else if(callback) {
-      ['onReady', 'onError'].forEach(function (funName) {
-        if(typeof callback[funName] === "function") {
-          callback[funName] = zone.bind(callback[funName]);
-        };
-      });
-    }
-    original_Meteor_subscribe.apply(this, args);
-  }
-}
+Meteor.subscribe = hijackSubscribe(original_Meteor_subscribe);
 
 var original_Cursor_observe = LocalCollection.Cursor.prototype.observe;
 LocalCollection.Cursor.prototype.observe = hijackCursor(original_Cursor_observe);
