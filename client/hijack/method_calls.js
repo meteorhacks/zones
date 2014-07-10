1ea86d5b5f7f82c275f493261b1a27dcc30d675d
Muhammed Thanish <mnmtanish@gmail.com>
2014-07-10 10:43:39 +0530

    Make sure the last argument is a function

--- client/hijack/method_calls.js
+++ client/hijack/method_calls.js
@@ -9,7 +9,9 @@ var original_Meteor_call = Meteor.call;
 Meteor.call = function() {
   var args = Array.prototype.slice.call(arguments);
   var callback = args[args.length - 1];
-  var zonedCallback = zone.bind(callback);
-  args[args.length - 1] = zonedCallback;
+  if(typeof callback === 'function') {
+    var zonedCallback = zone.bind(callback);
+    args[args.length - 1] = zonedCallback;
+  };
   return original_Meteor_call.apply(this, args);
 };
