c3539fe9daba0716dd3238b30fd0ad987d718e33
Muhammed Thanish <mnmtanish@gmail.com>
2014-07-10 16:21:32 +0530

    Refactor Connection.prototype.apply hijack

--- client/hijack.js
+++ client/hijack.js
@@ -5,20 +5,16 @@ var ConnectionProto = getConnectionProto();
  * Hijack method calls
  */
 var original_Connection_apply = ConnectionProto.apply;
-ConnectionProto.apply = function (name, args, options, callback) {
-  if(typeof callback === 'function') {
-    callback = zone.bind(callback);
-    original_Connection_apply.call(this, name, args, options, callback);
-  } else if(!callback && typeof options === 'function') {
-    // 3 arguments (name, args, callback)
-    callback = zone.bind(options);
-    original_Connection_apply.call(this, name, args, callback);
-  } else {
-    // 3 arguments (name, args, options)
-    callback = zone.bind(Function());
-    original_Connection_apply.call(this, name, args, options, callback);
-  }
-}
+ConnectionProto.apply = function () {
+  var args = Array.prototype.slice.call(arguments);
+  if(args.length) {
+    var callback = args[args.length - 1];
+    if(typeof callback === 'function') {
+      args[args.length - 1] = zone.bind(callback);
+      original_Connection_apply.apply(this, args);
+    };
+  };
+};
 
 /*
  * Hijack DDP subscribe method
