b02bb72c991236c1daeebb79df6715673bb1da28
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-08-22 18:48:40 +0530

    process zones asyncronously

--- assets/reporters.js
+++ assets/reporters.js
@@ -38,28 +38,34 @@ Zone.Reporters.longStackTrace = function (zone) {
   trace.push("Error: " + zone.erroredStack._e.message);
   trace.push(zone.erroredStack.get());
 
-  while (currZone && currZone.currentStack) {
-    var asyncTime = currZone.runAt - currZone.createdAt;
+  processZone();
   
-    if(prevZone) {
-      // sometimes, there are gaps betweens zones
-      // specially when handling with CPU intensive tasks and
-      // with handling maxDepth
-      var diff = prevZone.createdAt - currZone.runAt;
-      asyncTime += diff;
-    }
+  function processZone() {
+    if(currZone && currZone.currentStack) {
+      var asyncTime = currZone.runAt - currZone.createdAt;
+    
+      if(prevZone) {
+        // sometimes, there are gaps betweens zones
+        // specially when handling with CPU intensive tasks and
+        // with handling maxDepth
+        var diff = prevZone.createdAt - currZone.runAt;
+        asyncTime += diff;
+      }
 
-    if(asyncTime && asyncTime > 0) {
-      totalAsyncTime += asyncTime;
-      trace.push('\n> Before: ' + totalAsyncTime + 'ms (diff: ' + asyncTime + 'ms)');
-    }
+      if(asyncTime && asyncTime > 0) {
+        totalAsyncTime += asyncTime;
+        trace.push('\n> Before: ' + totalAsyncTime + 'ms (diff: ' + asyncTime + 'ms)');
+      }
 
-    trace.push(currZone.currentStack.get());
-    prevZone = currZone;
-    currZone = currZone.parent;
+      trace.push(currZone.currentStack.get());
+      prevZone = currZone;
+      currZone = currZone.parent;
+      
+      setTimeout(processZone, 0);
+    } else {
+      console.log(trace.join('\n'));
+    }
   }
-
-  console.log(trace.join('\n'));
 }
 
 Zone.Reporters.add('longStackTrace', Zone.Reporters.longStackTrace);
